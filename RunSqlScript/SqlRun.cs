b2509552728251309b0d611c8583596ead055081
Gary.zhang <Gary.zhang@finisar.com>
2018-01-05 15:49:41 +0800

    First commit

--- RunSqlScript/SqlRun.cs
+++ RunSqlScript/SqlRun.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RunSqlScript
 {
@@ -11,20 +7,25 @@ namespace RunSqlScript
         public static bool Run(string dataSource, string usrId, string pwd, string path, out string msg)
         {
             msg = "";
-            string constr = @"-S {0} -U {1} -P {2}  -i {3}";
+            const string constr = @"-S {0} -U {1} -P {2}  -i {3}";
             object[] param = { dataSource, usrId, pwd, path };
             try
             {
-                System.Diagnostics.Process sqlProcess = new System.Diagnostics.Process();
-                sqlProcess.StartInfo.FileName = "osql.exe ";
-                sqlProcess.StartInfo.UseShellExecute = false;
-                sqlProcess.StartInfo.CreateNoWindow = true;
-                sqlProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                sqlProcess.StartInfo.RedirectStandardOutput = true;
-                sqlProcess.StartInfo.Arguments = string.Format(constr, param);
+                var sqlProcess = new System.Diagnostics.Process
+                {
+                    StartInfo =
+                    {
+                        FileName = "osql.exe ",
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                        RedirectStandardOutput = true,
+                        Arguments = string.Format(constr, param)
+                    }
+                };
                 sqlProcess.Start();
                 sqlProcess.WaitForExit();
-                System.IO.StreamReader sr = sqlProcess.StandardOutput;
+                var sr = sqlProcess.StandardOutput;
                 msg = sr.ReadToEnd();
                 sqlProcess.Close();
 
